698e228f831a7c75f1c4ea4ebbf202e15fb4235e
Itzick Sabi <ItzickS@finonex.com>
2022-04-13 15:25:46 +0300

    added all files

---
+++ session2/src/assignment1.py
@@ -0,0 +1,11 @@
+def run(people_file_path, age_range_file_path):
+    pass
+    # TODO: Main logic should be placed here including reading the two files, reading `YAML_OUTPUT_PATH` env variable
+    # TODO: and creating the new folder + yaml file
+    # TODO: Please do not change the `run` method signature!!! this will cause your home assignment submission to fail.
+    # TODO: You are of course invited to extract other methods and constants as you like
+
+
+if __name__ == '__main__':
+    run("json_file", "json_file")
+    # TODO: Parse environment variables here and call `run(people_file_path, age_range_file_path):`
